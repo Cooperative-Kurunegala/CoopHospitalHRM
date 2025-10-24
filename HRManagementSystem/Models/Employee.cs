f4c295f47f1817181b458028727ee59c7e9e8e9f
Thimathi Manahara <manahara@ehealthcare.lk>
2025-10-24 21:58:32 +0530

    First Update

---
+++ HRManagementSystem/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace HRManagementSystem.Models;
+
+public class Employee
+{
+    public int id { get; set; }
+    public string fullName { get; set; }
+    public string nic { get; set; }
+    public string address { get; set; }
+    public string email { get; set; }
+    public string phone { get; set; }
+    public string designation { get; set; }
+    public string department { get; set; }
+    public string epfNumber { get; set; }
+    public string etfumber { get; set; }
+    public DateTime dateOfJoining { get; set; }
+    public string workSchedule { get; set; }
+    
+    public int? employeeCategoryId { get; set; }
+    public EmployeeCategory employeeCategory { get; set; }
+}
---
+++ HRManagementSystem/Models/EmployeeCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace HRManagementSystem.Models;
+
+public class EmployeeCategory
+{
+    public int id { get; set; }
+    public string categoryName { get; set; }
+    public ICollection<Employee> employees { get; set; }
+    
+}
