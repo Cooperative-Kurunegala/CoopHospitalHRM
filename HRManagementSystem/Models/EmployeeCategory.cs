0621db84e6932ee3d908d9ea6a271b5253a21c4c
eHealthcare Solutions <106938994+devs-ehealth@users.noreply.github.com>
2025-11-03 11:35:11 +0530

    Done
    Done

--- HRManagementSystem/Models/EmployeeCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRManagementSystem.Models;
-
-public class EmployeeCategory
-{
-    public int id { get; set; }
-    public string categoryName { get; set; }
-    public ICollection<Employee> employees { get; set; }
-    
-}
